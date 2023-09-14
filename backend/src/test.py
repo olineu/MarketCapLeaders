131f38a7ffcfa86cb4fadf53c139068f5b1d55a6
test <oneu1520@gmail.com>
2023-09-15 00:24:03 +0200

    edited test.py

--- backend/src/test.py
+++ backend/src/test.py
@@ -1,18 +1,28 @@
 import yfinance as yf
 from datetime import datetime, timedelta
+import pandas as pd
 
-# Create a Ticker object for the stock you want to retrieve data for
-ticker_symbol = "MSFT"  # Replace with the symbol of the stock you're interested in
-stock = yf.Ticker(ticker_symbol)
+# Define the stock symbol and create a Ticker object
+symbol = "MSFT"
+stock = yf.Ticker(symbol)
 
 # Calculate yesterday's date
-yesterday = datetime.now() - timedelta(days=1)
-date = yesterday.strftime("%Y-%m-%d")
+date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
 
-# Get historical market data for yesterday's date
-historical_data = stock.history(period="1d", start=date)
+# Get stock price from yesterday and extract the closing price (rounded to 2 decimal places)
+closing_price = round(stock.history(period="1d", start=date)["Close"].iloc[0], 2)
 
-# Extract the closing price for yesterday's date
-closing_price = historical_data["Close"].iloc[0]
+# Get the number of shares from a specific date
+num_shares = stock.get_shares_full(start="2022-01-01", end=None)
 
+# Extract the last value (rounded to 2 decimal places) and date from the Series
+last_value = round(num_shares.iloc[-1], 2)
+last_date = num_shares.index[-1].date()
+
+# Calculate market cap (rounded to 2 decimal places)
+market_cap = round(closing_price * last_value, 2)
+
+# Print the results
 print(f"Closing price on {date}: ${closing_price}")
+print(f"No. of shares on {last_date}: ${last_value}")
+print(f"Market cap on {date}: ${market_cap}")
